85fe6b0c434f393951ae66315004d8be02796abc
Aiden Cullo <aidencullo@gmail.com>
2024-08-29 10:17:33 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+// @ts-check
+
+interface Learner {
+  // Add an optional id property:
+  // id?: string;
+  name: {
+    first: string;
+    last: string;
+    // Make last optional using "last?"
+  };
+  course: string;
+  grades: number[];
+}
+
+const john: Learner = {
+  id: "1208365",
+  name: {
+    first: "John"
+  },
+  course: "Introduction to TypeScript",
+  grades: [96, 88, 100, 94]
+};
+
+function logLearner(l: Learner) {
+  console.log(
+    `Learner ${l.name.first} ${l.name.last} is currently enrolled in ${l.course}.`
+  );
+}
+
+logLearner(john);
